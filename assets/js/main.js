aa2aaf075d20e17950a243c9d9beb4df37ce569a
m3ro1986 <m3ro_1986@outlook.com>
2022-12-04 17:58:13 -0600

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,33 @@ const items = [
       quantity: 20
     }
   ]
+
+const themeBtn = document.getElementById( 'theme-btn' );
+const body = document.body
+const shopOpen = document.getElementById( 'shop-btn' )
+const shopClose = document.getElementById( 'close-cart' )
+const cartContainer = document.getElementById( 'cart-container' )
+
+themeBtn.addEventListener( 'click', e => {
+  body.classList.toggle('darktheme')
+
+  if( themeBtn.classList.contains( 'bx-moon' )){
+    themeBtn.classList.replace( 'bx-moon', 'bx-sun' )
+  }else{
+    themeBtn.classList.replace( 'bx-sun', 'bx-moon' )
+    }
+  })
+
+shopOpen.addEventListener( 'click', e => {
+  cartContainer.classList.remove( 'hide' )
+})
+
+shopClose.addEventListener( 'click', e => {
+  cartContainer.classList.add( 'hide' )
+})
+
+window.addEventListener('scroll', () => {
+  document.querySelector('header').classList.toggle('scroll-bar', window.scrollY > 0)
+  })
+
+
